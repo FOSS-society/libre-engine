b0098adc34c2d7f6c2f1220056cd50f600feadb6
James Clark <metallicsoul92@aim.com>
2016-09-16 17:04:54 -0400

    Added more to the OpenGL Namespace pt 2
    implemented functions that i declared in the previous commit

--- src/system/OpenGL.cpp
+++ src/system/OpenGL.cpp
@@ -12,13 +12,182 @@ namespace libre{
 		if (error != GL_NO_ERROR)
 		{
 
-      std::cerr << "OpenGL Error : "<< error << " during function " << function << " in file : " << file << " line: " << line;
-      return false;
+		std::cerr << "OpenGL Error : "<< error << " during function " << function << " in file : " << file << " line: " << line;
+      		return false;
 		}
 		return true;
-    }
+    		}
+
+	void genBuffers(int amount, unsigned* id)
+		{
+			glGenBuffers(amount, id);
+		}
+
+		void bindBuffers(GLenum type,unsigned id)
+		{
+			glBindBuffer(type, static_cast<GLuint>(id));
+		}
+		void deleteBuffer(unsigned* id)
+		{
+			glDeleteBuffers(1,id);
+		}
+
+		void deleteBuffer(int amount, unsigned* id)
+		{
+			glDeleteBuffers(amount, id);
+		}
+
+		void drawArrays(GLenum mode, int first, int count)
+		{
+			glDrawArrays(mode, first, count);
+		}
+
+		void drawElements(GLenum mode, int count, GLenum type, void* indices)
+		{
+			glDrawElements(mode, count, type, indices);
+		}
+
+		void BatchDrawArray(GLenum mode, GLint* first, int* count, int primcount)
+		{
+			glMultiDrawArrays(mode, first, count, primcount);
+		}
+
+		void VertexAttributePointer(unsigned index, int size, GLenum type, bool normalized, int stride, const void* offset)
+		{
+			glVertexAttribPointer(index, size, type, normalized, stride, offset);
+		}
+
+		void VertexAttributeIPointer(unsigned index, int size, GLenum type, int stride, const void* offset)
+		{
+			glVertexAttribIPointer(index, size, type, stride, offset);
+		}
+		void VertexAttributeLPointer(unsigned index, int size, GLenum type, int stride, const void* offset)
+		{
+			glVertexAttribLPointer(index, size, type, stride, offset);
+		}
+
+		void genVertexArrays(int size, unsigned* arrays)
+		{
+			glGenVertexArrays(size, arrays);
+		}
+
+		void deleteVertexArrays(int size, unsigned* arrays)
+		{
+			glDeleteVertexArrays(size, arrays);
+		}
+
+		void enableVertexArrays(unsigned index)
+		{
+			glEnableVertexAttribArray(index);
+		}
+
+		unsigned createShaderProgram()
+		{
+			return glCreateProgram();
+		}
+
+		unsigned createShader(GLenum type)
+		{
+			return glCreateShader(type);
+		}
+
+		void compileShader(unsigned shader)
+		{
+			glCompileShader(shader);
+		}
+
+		void setShaderSource(unsigned shader, int count, char* source, int* length)
+		{
+			glShaderSource(shader, count, &source, length);
+		}
+
+		void getShaderParameter(unsigned shader, GLenum pname, int* params)
+		{
+			glGetShaderiv(shader, pname, params);
+		}
+
+		void getShaderInfoLog(unsigned shader, int maxLength, int* length, char* infolog)
+		{
+			glGetShaderInfoLog(shader, maxLength, length, infolog);
+		}
+
+		
+
+		void deleteShader(unsigned shader)
+		{
+			glDeleteShader(shader);
+		}
+
+		void attachShader(unsigned program, unsigned shader)
+		{
+			glAttachShader(program, shader);
+		}
+
+		void linkProgram(unsigned program)
+		{
+			glLinkProgram(program);
+		}
+
+		void ValidateProgram(unsigned program)
+		{
+			glValidateProgram(program);
+		}
+
+		void genTextures(int amount, unsigned* textures)
+		{
+			glGenTextures(amount, textures);
+		}
+
+		void bindTexture(GLenum target, unsigned texture)
+		{
+			glBindTexture(target, texture);
+		}
+
+		void setTextureParameteri(GLenum target, GLenum pname, int param)
+		{
+			glTextureParameteri(target, pname, param);
+		}
 
 
+		void setTextureParameterf(GLenum target, GLenum pname, float param)
+		{
+			glTextureParameterf(target, pname, param);
+		}
+
+		void setTextureParameterip(GLenum target, GLenum pname, int* param)
+		{
+		glTextureParameteriv(target, pname, param);
+		}
+
+		void setTextureParameterfp(GLenum target, GLenum pname, float* param)
+		{
+		glTextureParameterfv(target, pname, param);
+		}
+
+		void defineTextureImage(GLenum target, int level, int internalformat, math::vec2<int> size, int border, GLenum format, GLenum type, void* data)
+		{
+			glTexImage2D(target, level, internalformat, size.x, size.y, border, format, type, data);
+		}
+
+		void deleteTexture(int amount, unsigned textures)
+		{
+			glDeleteTextures(amount, &textures);
+		}
+
+		void useProgram(unsigned program)
+		{
+			glUseProgram(program);
+		}
+
+		void BindVertexArray(unsigned id)
+		{
+			glBindVertexArray(id);
+		}
+
+		void createBufferData(GLenum target, GLsizeiptr size, const void* data, GLenum usage)
+		{
+			glBufferData(target, size, data, usage);
+		}
 
   }
 }
